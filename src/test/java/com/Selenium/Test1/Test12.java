47c456207f0a9a3cfd092c7b4eb2514c6c1824a9
spgorai@gmail.com <spgorai@gmail.com>
2017-11-27 00:30:50 +0530

    m

---
+++ src/test/java/com/Selenium/Test1/Test12.java
@@ -0,0 +1,13 @@
+package com.Selenium.Test1;
+
+import org.testng.annotations.Test;
+
+public class Test12 {
+	
+	@Test
+	public void GitTest()
+	{
+		System.out.println("My first poject in Git");
+	}
+
+}
